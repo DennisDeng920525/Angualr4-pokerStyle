a0991c3e7495ad4cef8cb9dbbad3f54bdcf567a1
dennis <281946572@qq.com>
2017-10-05 17:53:11 +0800

    add the prefect scroll bar to system.

--- src/app/style-demo/display/flex/flex.component.ts
+++ src/app/style-demo/display/flex/flex.component.ts
@@ -16,61 +16,67 @@ export class FlexComponent implements OnInit {
       name: 'flex-direction属性',
       field: 'flex-direction',
       values: [
-        { text: 'row', isActive: false, tooltip: '' },
-        { text: 'row-revers', isActive: false, tooltip: '' },
-        { text: 'column', isActive: false, tooltip: '' },
-        { text: 'column-revers', isActive: false, tooltip: '' }],
-      tooltip: 'flex-direction属性决定主轴的方向（即项目的排列方向）。'
+        { text: 'row', isActive: false, tooltip: '（默认值）主轴为水平方向，起点在左端' },
+        { text: 'row-reverse', isActive: false, tooltip: '主轴为水平方向，起点在右端' },
+        { text: 'column', isActive: false, tooltip: '主轴为垂直方向，起点在上沿' },
+        { text: 'column-reverse', isActive: false, tooltip: '主轴为垂直方向，起点在下沿' }],
+      introduce: 'flex-direction属性决定主轴的方向（即项目的排列方向）。',
+      showIntroduce: false
     },
     {
       name: 'flex-wrap属性',
       field: 'flex-wrap',
       values: [
-        { text: 'nowrap', isActive: false, tooltip: '123' },
-        { text: 'wrap', isActive: false, tooltip: '' },
-        { text: 'wrap-revers', isActive: false, tooltip: '' }],
-      tooltip: '默认情况下，项目都排在一条线（又称"轴线"）上。flex-wrap属性定义，如果一条轴线排不下，如何换行。'
+        { text: 'nowrap', isActive: false, tooltip: '不换行' },
+        { text: 'wrap', isActive: false, tooltip: '换行，第一行在上方' },
+        { text: 'wrap-reverse', isActive: false, tooltip: '换行，第一行在下方' }],
+      introduce: '默认情况下，项目都排在一条线（又称"轴线"）上。flex-wrap属性定义，如果一条轴线排不下，如何换行。',
+      showIntroduce: false
     },
     {
       name: 'flex-flow属性',
       field: 'flex-flow',
       values: [
         { text: 'row nowrap', isActive: false, tooltip: '' }],
-      tooltip: 'flex-flow属性是flex-direction属性和flex-wrap属性的简写形式，默认值为row nowrap。'
+      introduce: 'flex-flow属性是flex-direction属性和flex-wrap属性的简写形式，默认值为row nowrap。',
+      showIntroduce: false
     },
     {
       name: 'justify-content属性',
       field: 'justify-content',
       values: [
-        { text: 'flex-start', isActive: false, tooltip: '' },
-        { text: 'flex-end', isActive: false, tooltip: '' },
-        { text: 'center', isActive: false, tooltip: '' },
-        { text: 'space-between', isActive: false, tooltip: '' },
-        { text: 'space-around', isActive: false, tooltip: '' }],
-      tooltip: 'justify-content属性定义了项目在主轴上的对齐方式。'
+        { text: 'flex-start', isActive: false, tooltip: '（默认值）左对齐' },
+        { text: 'flex-end', isActive: false, tooltip: '右对齐' },
+        { text: 'center', isActive: false, tooltip: '居中' },
+        { text: 'space-between', isActive: false, tooltip: '两端对齐，项目之间的间隔都相等' },
+        { text: 'space-around', isActive: false, tooltip: '每个项目两侧的间隔相等。所以，项目之间的间隔比项目与边框的间隔大一倍' }],
+      introduce: 'justify-content属性定义了项目在主轴上的对齐方式。',
+      showIntroduce: false
     },
     {
       name: 'align-itemsn属性',
       field: 'align-items',
       values: [
-        { text: 'flex-start', isActive: false, tooltip: '' },
-        { text: 'flex-end', isActive: false, tooltip: '' },
-        { text: 'center', isActive: false, tooltip: '' },
-        { text: 'baseline', isActive: false, tooltip: '' },
-        { text: 'stretch', isActive: false, tooltip: '' }],
-      tooltip: 'align-items属性定义项目在交叉轴上如何对齐。'
+        { text: 'flex-start', isActive: false, tooltip: '交叉轴的起点对齐' },
+        { text: 'flex-end', isActive: false, tooltip: '交叉轴的终点对齐' },
+        { text: 'center', isActive: false, tooltip: '交叉轴的中点对齐' },
+        { text: 'baseline', isActive: false, tooltip: '项目的第一行文字的基线对齐' },
+        { text: 'stretch', isActive: false, tooltip: '（默认值）如果项目未设置高度或设为auto，将占满整个容器的高度' }],
+      introduce: 'align-items属性定义项目在交叉轴上如何对齐。',
+      showIntroduce: false
     },
     {
       name: 'align-content属性',
       field: 'align-content',
       values: [
-        { text: 'flex-start', isActive: false, tooltip: '' },
-        { text: 'flex-end', isActive: false, tooltip: '' },
-        { text: 'center', isActive: false, tooltip: '' },
-        { text: 'space-between', isActive: false, tooltip: '' },
-        { text: 'space-around', isActive: false, tooltip: '' },
-        { text: 'stretch', isActive: false, tooltip: '' }],
-      tooltip: 'align-content属性定义了多根轴线（多行）的对齐方式。如果项目只有一根轴线，该属性不起作用。'
+        { text: 'flex-start', isActive: false, tooltip: '与交叉轴的起点对齐' },
+        { text: 'flex-end', isActive: false, tooltip: '与交叉轴的终点对齐' },
+        { text: 'center', isActive: false, tooltip: '与交叉轴的中点对齐' },
+        { text: 'space-between', isActive: false, tooltip: '与交叉轴两端对齐，轴线之间的间隔平均分布' },
+        { text: 'space-around', isActive: false, tooltip: '每根轴线两侧的间隔都相等。所以，轴线之间的间隔比轴线与边框的间隔大一倍' },
+        { text: 'stretch', isActive: false, tooltip: '（默认值）轴线占满整个交叉轴' }],
+      introduce: 'align-content属性定义了多根轴线（多行）的对齐方式。如果项目只有一根轴线，该属性不起作用。',
+      showIntroduce: false
     }
   ];
 
@@ -131,5 +137,9 @@ export class FlexComponent implements OnInit {
         this.styleWindow.push({ field: index, value: style });
       }
     });
+  }
+
+  switchIntroduce(property) {
+    property.showIntroduce = !property.showIntroduce;
   }
 }
--- src/app/style-demo/style-demo.module.ts
+++ src/app/style-demo/style-demo.module.ts
@@ -3,6 +3,12 @@ import { CommonModule } from '@angular/common';
 import { FlexComponent } from './display/flex/flex.component';
 import { BaseModule } from '../base/base.module';
 import { TooltipModule, TabsModule } from 'ngx-bootstrap';
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
+
+const PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+  // suppressScrollX: true
+};
 
 @NgModule({
   declarations: [
@@ -11,6 +17,7 @@ import { TooltipModule, TabsModule } from 'ngx-bootstrap';
   imports: [
     TooltipModule.forRoot(),
     TabsModule.forRoot(),
+    PerfectScrollbarModule.forRoot(PERFECT_SCROLLBAR_CONFIG),
     CommonModule,
     BaseModule
   ],
